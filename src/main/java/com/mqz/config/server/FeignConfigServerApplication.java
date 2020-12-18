0c68c4031a55bda43836be8df6a0c723ef8e7240
蒙启章 <mengqizhang@huluwa.cc>
2020-12-18 15:52:21 +0800

    分布式配置中心

---
+++ src/main/java/com/mqz/config/server/FeignConfigServerApplication.java
@@ -0,0 +1,25 @@
+package com.mqz.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author mqz
+ * @since 2020年12月18日
+ * @description 分布式配置中心-服务端
+ * @abount https://github.com/DemoMeng
+ */
+@EnableEurekaClient
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class FeignConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mqz/config/server/FeignConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mqz.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
